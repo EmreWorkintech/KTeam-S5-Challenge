70889dd89845077d3a90b62f91809127baf327a6
Ceylin Yasar <ceylinrukiyeyasar@gmail.com>
2022-12-30 17:16:54 +0300

    tablar bitti

--- src/bileşenler/tablar.js
+++ src/bileşenler/tablar.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Tablar = (konu) => {
   // GÖREV 3
   // ---------------------
@@ -30,6 +32,11 @@ const Tablar = (konu) => {
 };
 
 const tabEkleyici = (secici) => {
+  const seciciEkle = document.querySelector(secici);
+  axios.get("http://localhost:5001/api/konular").then((response) => {
+    seciciEkle.appendChild(Tablar(response.data.konular));
+  });
+
   // GÖREV 4
   // ---------------------
   // Tek argümanı olarak bir css seçici alan bu işlevi uygulayın.
